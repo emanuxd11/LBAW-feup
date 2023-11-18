fc1ec8e4d2786e98eaac1bb651f6d2ad5f10f0ea
Emanuel Maia <tavanomanu@gmail.com>
2023-11-18 00:41:35 +0000

    Remove template-laravel

--- template-laravel/storage/framework/views/0d3ebb696bdf602b6de44d706f974656.php
+++
@@ -1,7 +0,0 @@
-<li class="item" data-id="<?php echo e($item->id); ?>">
-    <label>
-        <input type="checkbox" <?php echo e($item->done?'checked':''); ?>>
-        <span><?php echo e($item->description); ?></span>
-        <a href="#" class="delete">&#10761;</a>
-    </label>
-</li><?php /**PATH /home/manu/UNI/3_ano/LBAW-feup/template-laravel/resources/views/partials/item.blade.php ENDPATH**/ ?>
