218e7a19a2662a4a4380541c046a3d3c99313226
Alberto Serra <alberto-serra@DESKTOP-SPQVST4.localdomain>
2023-11-19 01:30:19 +0000

    Header added with new FAQ and About Us Page added

--- ProjectSync/resources/views/layouts/app.blade.php
+++ ProjectSync/resources/views/layouts/app.blade.php
@@ -1,37 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link href="{{ url('css/milligram.min.css') }}" rel="stylesheet">
-        <link href="{{ url('css/app.css') }}" rel="stylesheet">
-        <script type="text/javascript">
-            // Fix for Firefox autofocus CSS bug
-            // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-        </script>
-        <script type="text/javascript" src={{ url('js/app.js') }} defer>
-        </script>
-    </head>
-    <body>
-        <main>
-            <header>
-                <h1><a href="{{ url('/projects') }}">ProjectSync</a></h1>
-                @if (Auth::check())
-                    <a class="button" href="{{ url('/logout') }}"> Logout </a>
-                    <a href="{{ route('profilePage', ['username' => Auth::user()->username]) }}"><span>{{ Auth::user()->name }}</span></a>
-                @endif
-            </header>
-            <section id="content">
-                @yield('content')
-            </section>
-        </main>
-    </body>
+    <!-- Styles -->
+    <link href="{{ url('css/milligram.min.css') }}" rel="stylesheet">
+    <link href="{{ url('css/app.css') }}" rel="stylesheet">
+    <script type="text/javascript" src="{{ url('js/app.js') }}" defer></script>
+</head>
+<body>
+    <main>
+        @include('partials.header')
+        <section id="content">
+            @yield('content')
+        </section>
+    </main>
+</body>
 </html>
---
+++ ProjectSync/resources/views/pages/aboutUs.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/aboutUs.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <!-- Your about us page content goes here -->
+    <h1>About Us</h1>
+    <p>Welcome to our about us page!</p>
+@endsection
