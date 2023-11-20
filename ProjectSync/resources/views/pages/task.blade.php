f5d07bb373e4f02c1189446ff2e28fac3f884d4f
Miguel <up202108822@up.pt>
2023-11-20 22:50:27 +0000

    fixed task editing

--- ProjectSync/resources/views/pages/task.blade.php
+++ ProjectSync/resources/views/pages/task.blade.php
@@ -42,11 +42,28 @@
         </div>
 
         <div class="task-actions">
+            <h3 class="edit-task">Edit Task</h3>
             <form method="POST" action="{{ route('edit_task',['id' => $task->id]) }}">
                 @method('POST')
                 @csrf
+                <p class="info-label">Name:</p>
+                <input type="text" name="name">
+                <p class="info-label">Description:</p>
+                <input type="text" name="description">
+                <p class="info-label">Status:</p>
+                <select id="status" name="status">
+                    <option value="" selected disabled>{{$task->status}}</option>
+                    <option value="To Do">To Do</option>
+                    <option value="Doing">Doing</option>
+                    <option value="Done">Done</option>
+                </select>
+                <p class="info-label">Delivery Date:</p>
+                <input type="date" name="delivery_date">
+                <p class="info-label">Add user:</p>
+                <input type="text" name="username">
                 <button type="submit" class="button edit-button"><i class="fas fa-edit"></i> Edit</button>
             </form>
+        
 
             <form method="POST" action="{{ route('delete_task',['id' => $task->id]) }}">
                 @method('DELETE')
