669e54a3deeaa9335e0fdd55bbab220bc0c60714
Alberto Serra <alberto-serra@DESKTOP-SPQVST4.localdomain>
2023-11-20 02:55:11 +0000

    Login frontend done

--- ProjectSync/resources/views/auth/login.blade.php
+++ ProjectSync/resources/views/auth/login.blade.php
@@ -1,37 +1,61 @@
+<!-- login.blade.php -->
+
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
+    <div class="container mt-5">
+        <div class="login-container">
+            <div class="card login-card">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}" class="login-form">
+                        {{ csrf_field() }}
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus class="form-control">
+                            @if ($errors->has('email'))
+                                <span class="error">
+                                    {{ $errors->first('email') }}
+                                </span>
+                            @endif
+                        </div>
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
-            {{ $errors->first('password') }}
-        </span>
-    @endif
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input id="password" type="password" name="password" required class="form-control">
+                            @if ($errors->has('password'))
+                                <span class="error">
+                                    {{ $errors->first('password') }}
+                                </span>
+                            @endif
+                        </div>
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} class="form-check-input">
+                            <label class="form-check-label remember-text">Remember Me</label>
+                        </div>
 
-    <button type="submit">
-        Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
-    @if (session('success'))
-        <p class="success">
-            {{ session('success') }}
-        </p>
-    @endif
-</form>
+                        <button type="submit" class="btn btn-primary">
+                            Login
+                        </button>
+
+                        <div class="separator text-center">
+                            <hr>
+                            <span id="no-account">Don't have an account?</span>
+                            <hr>
+                        </div>
+
+                        <a class="btn btn-primary btn-register register-button" href="{{ route('register') }}">
+                            Register
+                        </a>
+                        @if (session('success'))
+                            <p class="success">
+                                {{ session('success') }}
+                            </p>
+                        @endif
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
