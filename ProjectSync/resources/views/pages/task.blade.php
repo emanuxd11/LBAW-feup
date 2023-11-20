5931b4dee0635357eaaae7c01b04cff01f654529
Miguel <up202108822@up.pt>
2023-11-20 17:50:10 +0000

    added options to status

--- ProjectSync/resources/views/pages/task.blade.php
+++ ProjectSync/resources/views/pages/task.blade.php
@@ -9,11 +9,19 @@
     <p>Task started on {{$task->start_date}}</p>
     <p>Needs to be done by {{$task->delivery_date}}</p>
     <p>Status: {{$task->status}}</p>
+    <p>Assigned to: {{$task->status}}</p>
     <form method="POST" action="{{ route('edit_task',['id' => $task->id]) }}">
         @method('POST')
         @csrf
         Name: <input type="text" name="name">
         Description: <input type="text" name="description">
+        <label for="status">Status:</label>
+        <select id="status" name="status">
+            <option value="" selected disabled>{{$task->status}}</option>
+            <option value="To Do">To Do</option>
+            <option value="Doing">Doing</option>
+            <option value="Done">Done</option>
+        </select>
         Delivery Date: <input type="date" name="delivery_date">
         Add user: <input type="text" name="username">
         <button type="submit" class="button">+</button>
