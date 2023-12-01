8bcee1be35e739e6b69adbbd9f5359a4c7bd9fa7
Rúben Fonseca <rubenf_11@DESKTOP-Q9MVK7C.localdomain>
2023-12-01 19:13:00 +0000

    now is possible to have a profile pic

--- ProjectSync/resources/views/pages/profilePage.blade.php
+++ ProjectSync/resources/views/pages/profilePage.blade.php
@@ -7,43 +7,63 @@
 @section('content')
 
 <div id="ProfilePage">
-        <div class="currentUser">
-            <div class="col-md-8 offset-md-2">
-                <div class="card">
-                    <div class="card-header">{{ $user->name }}'s Profile</div>
+    <div class="currentUser">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">{{ $user->name }}'s Profile</div>
 
-                    <div class="card-body">
-                        <p>Username: {{ $user->username }}</p>
-                        <p>Email: {{ $user->email }}</p>
-                        <p>Phone Number: {{ $user->phonenumber ?? 'N/A'}}</p>
+                <div class="card-body">
 
-                        @if($user->isdeactivated)
-                            <p>Status: Deactivated</p>
-                        @else
-                            <p>Status: Activated</p>
-                        @endif
+                    <div class="profile-info">
+                        <div class="profile-details">
+                            <p>Username: {{ $user->username }}</p>
+                            <p>Email: {{ $user->email }}</p>
+                            <p>Phone Number: {{ $user->phonenumber ?? 'N/A'}}</p>
 
-                        @if(Auth::check() && (Auth::user()->is($user) || Auth::user()->isAdmin))
-                            <a href="{{ route('editProfile', ['username' => $user->username]) }}" class="btn btn-primary">Edit Profile</a>
-                        @endif
+                            @if($user->isdeactivated)
+                                <p>Status: Deactivated</p>
+                            @else
+                                <p>Status: Activated</p>
+                            @endif
+
+                            @if(Auth::check() && (Auth::user()->is($user) || Auth::user()->isAdmin))
+                                <a href="{{ route('editProfile', ['username' => $user->username]) }}" class="btn btn-primary">Edit Profile</a>
+                            @endif
+
+                        </div>
+
+                        <div class="profile-image">
+                            @if($user->profile_pic !== null && $user->profile_pic !== '')
+                                <img src="{{ asset('storage/' . $user->profile_pic) }}" alt="Profile Picture">
+                            @else
+                                <img src="{{ asset('storage/profile_pictures/default-profile-pic.jpg') }}" alt="Default Profile Picture">
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="bio-class">
+                        <h3>Bio</h3>
+                        <p>{{ $user->bio }}</p>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="currentProjects">
-            <div class="col-md-8 offset-md-2">
-                <h3>Projects</h3>           
-                <div class="showProjects">
-                    @forelse ($user->projects as $project)
-                        <div class="user">
-                            <h4>{{$project->name}}</h4>
-                        </div>
-                    @empty
-                        <h4>No projects found</h4>
-                    @endforelse
-                </div>
+    <div class="currentProjects">
+        <div class="col-md-8 offset-md-2">
+            <h3>Projects</h3>           
+            <div class="showProjects">
+                @forelse ($user->projects as $project)
+                    <div class="user">
+                        <h4>{{$project->name}}</h4>
+                    </div>
+                @empty
+                    <h4>No projects found</h4>
+                @endforelse
             </div>
         </div>
     </div>
+</div>
+
 @endsection
