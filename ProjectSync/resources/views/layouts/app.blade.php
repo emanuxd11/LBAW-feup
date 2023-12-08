5e27e1e9a8701f31a979760f7cbd380a679f5a9d
Emanuel Maia <tavanomanu@gmail.com>
2023-12-08 21:56:53 +0000

    Add footer.

--- ProjectSync/resources/views/layouts/app.blade.php
+++ ProjectSync/resources/views/layouts/app.blade.php
@@ -30,6 +30,10 @@
             @yield('content')
         </section>
     </main>
+
+    <footer>
+        <p>&copy; {{ date('Y') }} ProjectSync. All rights reserved.</p>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
