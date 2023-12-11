d5ae09de1f79c6f939dee7de3720d569fa1cfa20
Alberto Serra <alberto-serra@DESKTOP-SPQVST4.localdomain>
2023-12-11 18:51:46 +0000

    Tasks lists

--- ProjectSync/public/js/search_tasks.js
+++ ProjectSync/public/js/search_tasks.js
@@ -31,26 +31,32 @@ document.addEventListener('DOMContentLoaded', function () {
             var taskItem = document.createElement('li');
             taskItem.className = 'task';
             taskItem.setAttribute('data-id', task.id);
-
+    
+            var taskCard = document.createElement('div');
+            taskCard.className = 'task-card-search';
+    
             var taskLink = document.createElement('a');
             taskLink.href = `/api/projects/${project_id}/task/${task.id}`;
             taskLink.innerHTML = `<h3>${task.name}</h3>`;
-
-            taskLink.addEventListener('click', function (event) {
-                event.preventDefault();
-                window.location.href = taskLink.href;
-            });
-
-            taskItem.appendChild(taskLink);
-
-            // Set the HTML content for the task element
-            taskItem.innerHTML += `
+    
+            taskCard.appendChild(taskLink);
+    
+            var taskDetails = document.createElement('div');
+            taskDetails.innerHTML = `
                 <p>Description: ${task.description}</p>
                 <p>Task started on ${task.start_date}</p>
                 <p>Needs to be done by ${task.delivery_date ?? "No delivery date available"}</p>
                 <p>Status: ${task.status}</p>
             `;
-
+    
+            taskCard.appendChild(taskDetails);
+    
+            // Update the event listener to attach it to taskItem
+            taskItem.addEventListener('click', function () {
+                window.location.href = taskLink.href;
+            });
+    
+            taskItem.appendChild(taskCard);
             searchResults.appendChild(taskItem);
         });
     }
